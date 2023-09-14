fa4709209d766d5dd5cb0ecc13488ba9fc0a9855
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-09-14 12:44:54 +0200

    viendo la escritura de las variables

---
+++ tipos/variables.py
@@ -0,0 +1,17 @@
+"""
+Este es un ejemplo de un archivo de Python con un docstring de módulo.
+Descripción del propósito del módulo y cualquier información relevante.
+
+"""
+
+
+nombre_curso = "Ultimate Python"
+nombre2 = "Irina"
+Nombre_CurSo = " Python"
+NombreCurso = "Escribo varias variables"
+print(nombre_curso, nombre2, Nombre_CurSo, NombreCurso)
+
+alumnos = 6000
+puntuacion = 9.9 
+publicado = True 
+print(alumnos, puntuacion, publicado)
