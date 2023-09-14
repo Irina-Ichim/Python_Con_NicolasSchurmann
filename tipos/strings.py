fb74a5a520987fd0b8defed6e5a4b60be9002b3c
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-09-14 13:37:09 +0200

    strings, .len + posiciones

---
+++ tipos/strings.py
@@ -0,0 +1,25 @@
+"""
+Este es un ejemplo de un archivo de Python con un docstring de módulo.
+Descripción del propósito del módulo y cualquier información relevante.
+"""
+
+# Aquí puedes escribir el resto de tu código
+
+escribir_strings = "Ultimate Python"
+descripcion_curso = """
+Ultimate Python,
+este curso contempla todos los detalles que necesitas aprender para encontrar un trabajo como programador
+"""
+
+print(escribir_strings)
+print(descripcion_curso)
+
+print(len(escribir_strings))
+print(escribir_strings[0])
+
+nombre = "Gilda Irina Ichim"
+print(nombre[6:11])
+print(nombre[12:])
+print(nombre[6:])
+print(nombre[:11])
+print(nombre[:])
