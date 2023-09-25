ac6a7402e0576cc248a48c73480608e761b22687
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-09-25 11:25:22 +0200

    operador de formatear strings

---
+++ tipos/formatos-strings.py
@@ -0,0 +1,10 @@
+"""
+Este script muestra cómo crear un nombre completo a partir de un nombre y un apellido, 
+y cómo crear una cadena de texto utilizando una f-string.
+"""
+nombre = "Irina"
+apellido = "Ichim"
+nombre_completo = nombre + " " + apellido
+print(nombre_completo)
+todo_nombre = f"{nombre[1]} {2 + 5}"
+print(todo_nombre)
