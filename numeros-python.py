f9790b478e106a4e0fbb893c2ced90eaf3b173c9
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-10-04 20:59:01 +0200

    numeros int, float y imaginarios

---
+++ numeros-python.py
@@ -0,0 +1,19 @@
+numero = 2  # emtero -> integer
+decimal = 1.2  # float
+imaginario = 2 + 2j  # 2+ 2i
+
+#numero += 2
+#numero -= 2
+#numero *= 2
+#numero /= 2
+
+print("Mi numero es:", numero) 
+
+print(1 + 3)
+
+print(1 * 3)
+print(1 - 3)
+print(1 / 3)
+print(1 // 3)   # Solo nos muestra el entero pero sin decimales
+print(1 % 3)    # muestra el decimal
+print(2**2)
