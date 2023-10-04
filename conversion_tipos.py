8675060b78acfc42ba65a3c2f1d19dd7a8d0db9c
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-10-05 00:28:03 +0200

    explicando int, str, float y bool con sus tipos de resultados

---
+++ conversion_tipos.py
@@ -0,0 +1,13 @@
+# x = input("")
+
+# int()
+# str()
+# float()
+# bool() Los datos que nos devolvera falsy son ""(string vacio), numero 0 y none .
+# El resto de datos en la funcion bool() dan truthy
+
+print(bool(""))
+print(bool("0"))
+print(bool(None))
+print(bool(" "))
+print(bool(0))
