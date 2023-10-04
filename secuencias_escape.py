3ecee2b2d44b155f943b0384699f48ecbd43f4a1
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-10-04 20:28:33 +0200

    secuencias de escape en python

---
+++ secuencias_escape.py
@@ -0,0 +1,9 @@
+# Texto comentado o comentarios generales en Python
+#\"  Para poder usar las comillas dobles
+#\' Imprime las comillas simples
+# \\ Imprime el \
+# \n la segunda palabra la escribe abajo, en columna
+
+
+curso = "Ultimate \nPython\""
+print(curso)
