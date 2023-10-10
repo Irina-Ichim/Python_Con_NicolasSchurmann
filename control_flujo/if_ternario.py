4cea2a311282c05be735d194bbb509254e74efc6
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-10-10 14:04:36 +0200

    if ternario

---
+++ control_flujo/if_ternario.py
@@ -0,0 +1,15 @@
+# usa ctrl + ç para comentar todo el texto seleccionado
+# El valor ternario lo que hace es asignar un valor a una variable
+
+edad = 15 
+
+mensaje = "Es mayor" if edad > 17 else "Es menor"
+
+print (mensaje)
+
+# if edad > 26:
+#     mensaje = "mayor"
+# else:
+#     mensaje = "menor"
+    
+# print(mensaje)
