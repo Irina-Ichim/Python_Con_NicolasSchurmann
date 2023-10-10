c89a9e317e6fbe3dfd8f15628b5af5887b9fc14a
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-10-10 13:26:02 +0200

    if, elif y else

---
+++ control_flujo/if.py
@@ -0,0 +1,14 @@
+# Para que funcione correctamente debes identar
+# Las evaluaciones se hacen de arriba hacia abajo
+# Para que no tenga errores debo pensar muy bien las condiciones
+# Puedes usar varios elif y no necesariamente debes usar else
+edad = 72
+#edad = 15
+if edad > 54:
+    print("Puede ver la pelicula con descuento")
+elif edad > 17:
+    print("Puedes ver la pelicula")
+else:
+    print("No puedes entrar")
+    
+print("Listo")   
