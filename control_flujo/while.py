68d64a790fcc189b80e0b1e1ca56ad232aee060a
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-10-18 20:32:30 +0200

    while

---
+++ control_flujo/while.py
@@ -0,0 +1,10 @@
+# numero = 1
+# while numero < 100:
+#     print(numero)
+#     numero *= 2    # condicion
+
+comando = ""
+
+while comando != "salir":
+    comando = input("$ ")
+    print(comando)
