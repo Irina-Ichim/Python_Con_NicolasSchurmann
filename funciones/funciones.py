58a501c5ee917026bf9d1a827b563d2c0ce6c356
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-10-22 20:43:45 +0200

    funcionesz con parametros opcionales

--- funciones/funciones.py
+++ funciones/funciones.py
@@ -6,3 +6,12 @@ def hola(nombre, apellido):
 hola("Irina", "Irina")      # estoy llamando a la funcion / Irina es un valor y por lo tanto un argumento
 hola("Chanchito" ,"Feliz")
 
+# Pasamos parametros por defecto
+
+def persona(nombre, ciudad="Barcelona"):
+    print("Hola Mundo")
+    print(f"Bienvenida {nombre} {ciudad}")
+    
+    
+persona("irina", "ichim")
+persona("chanchito")
