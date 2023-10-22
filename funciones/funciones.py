3504b91fc20756a10fd9869c532721ab80bee68e
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-10-22 20:36:24 +0200

    funciones, parametros y argumentos

---
+++ funciones/funciones.py
@@ -0,0 +1,8 @@
+def hola(nombre, apellido):
+    print("Hola Mundo")
+    print(f"Bienvenida {nombre} {apellido}")   # nombre es parametro
+
+
+hola("Irina", "Irina")      # estoy llamando a la funcion / Irina es un valor y por lo tanto un argumento
+hola("Chanchito" ,"Feliz")
+
