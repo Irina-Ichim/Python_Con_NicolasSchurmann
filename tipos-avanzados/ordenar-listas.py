6b87e14eb8ec8f008a13b1734e5eb7ab95135e90
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-11-01 15:42:45 +0100

    metodo sort(), sorted(), reverse=True para orden descendente

---
+++ tipos-avanzados/ordenar-listas.py
@@ -0,0 +1,13 @@
+numeros = [4, 6, 8, 15, 55, 71, 66, 90]
+
+# numeros.sort() # ordena los elementos
+# numeros.sort(reverse=True)  # orden descedente
+
+numeros2 = sorted(numeros, reverse=True)  # sorted devuelve una nueva lista
+print(numeros)
+print(numeros2)
+
+
+usuarios = [[4, "Chanchito"], [1, "King"], [5, "Pulga"]]
+usuarios.sort()
+print(usuarios)
