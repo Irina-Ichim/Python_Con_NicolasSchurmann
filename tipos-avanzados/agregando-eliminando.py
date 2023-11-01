c466d3b74bcd8c0180b816df524b33745b514398
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-11-01 15:24:18 +0100

    metodos: insert(), append(), remove(), pop(), del, clear()

---
+++ tipos-avanzados/agregando-eliminando.py
@@ -0,0 +1,19 @@
+mascotas = [ 
+            "Chanchito Feliz",
+            "King",
+            "Chuchy",
+            "King",
+            "Pelusa",
+            "Pulga"]
+
+mascotas.insert(1, "Melvin") # agrega un nuevo elemento en la posicion que le indicas
+mascotas.append("Chanchito triste") # agrega un elemento al final de una lista
+
+
+
+mascotas.remove("King")  # remove solo elimina el primer elemento
+mascotas.pop()    # elimina el ultimo elemento de tu lista
+# mascotas.pop(indice que quieres eliminar)  te elimina el elemeto posicionado en el indice indicado
+del mascotas[5]
+# clear() devuelve un listado completamente vacio
+print(mascotas)
