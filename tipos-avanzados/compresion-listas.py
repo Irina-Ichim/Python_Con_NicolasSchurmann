453f043d9534c8d4d9895f43a148e2bc8013faef
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-11-01 22:11:19 +0100

    funciones map +  filter

--- tipos-avanzados/compresion-listas.py
+++ tipos-avanzados/compresion-listas.py
@@ -11,11 +11,16 @@ usuarios = [
 # print(nombres)
 
 # Forma más elegante
+# map
 # nombres = [usuario[0] for usuario in usuarios]   # usuario[0] le hemos aplicado una transformacion
 
-# filtrar
+# filtrar_ filter
 # nombres = [usuario for usuario in usuarios if usuario[1] > 2]
 
 #filtrar y transformar
-nombres = [usuario[0] for usuario in usuarios if usuario[1] > 2]
-print(nombres)
+# nombres = [usuario[0] for usuario in usuarios if usuario[1] > 2]
+
+# nombres = list(map(lambda usuario: usuario[0], usuarios))
+
+menosUsuarios = list(filter(lambda usuario: usuario[1] > 2, usuarios))
+print(menosUsuarios)
