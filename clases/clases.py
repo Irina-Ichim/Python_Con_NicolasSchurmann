2f9cefe91780df314c0f2c3b6837630e33c82353
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-11-11 00:30:36 +0100

    clases, instancias + isinstance()

---
+++ clases/clases.py
@@ -0,0 +1,10 @@
+class MiPerro:      # la primera letra debe ser mayuscula
+    def  habla(self):    # ojo con la identacion, estas funciones en una clase dejan de ser funciones, son metodos. Siempre el primer parametro de una class es self
+         print("Guau!") 
+       
+mi_perro = MiPerro()    # mi_perro es una instancia(=objeto)
+# print(type(mi_perro))
+mi_perro.habla()        # invocamos el metodo
+print(isinstance(mi_perro, MiPerro))   #isistence se usa para verificar si es una instancia, puede devolver true o false
+print(isinstance(mi_perro, str))
+        
---
+++ clases/introduccion.py
@@ -0,0 +1,6 @@
+mensaje = " Hola Mundo"
+
+print(type(mensaje)) # type te indica que tipo de clase es
+
+# Clase : es un plano de construccion
+# Objeto: es una instancia de una clase
