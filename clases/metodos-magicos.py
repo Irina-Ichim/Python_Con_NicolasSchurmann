b65d5a68ccb3ed7ec04814369ba72fff41ca9229
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-12-05 11:17:42 +0100

    destructor

--- clases/metodos-magicos.py
+++ clases/metodos-magicos.py
@@ -4,6 +4,9 @@ class Perro:
     def __init__(self, nombre, edad):
         self.nombre = nombre
         self.edad = edad
+        
+    def __del__(self):
+        print(f"Chao perro :( {self.nombre}")
 
     def __str__(self):
         return f"Clase Perro: {self.nombre}"
@@ -12,10 +15,13 @@ class Perro:
         print(f"{self.nombre} dice: Guau!")
 
 perro = Perro("Chanchito", 7)
-print(perro)
-texto = str(perro)
-print(texto)
+del perro
+# print(perro)
+# texto = str(perro)
+# print(texto)
 
 
 # perro.__     Te muestra el listado de todos los metodos magicos
 # Toda la información sobre los metodos magicos https://rszalski.github.io/magicmethods/
+# El constructor es un metodo magico que se ejecuta cuando creamos un objeto
+# El destructor es un metodo magico que se ejecuta cuando eliminamos un objeto
