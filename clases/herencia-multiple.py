9af7c4740156f50a77c851a248a8e2f38febbc2c
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-12-06 12:57:01 +0100

    herencia multiple

---
+++ clases/herencia-multiple.py
@@ -0,0 +1,19 @@
+class Caminador:
+    def caminar(self):
+        print("cominando")
+        
+
+class Volador:
+    def volar(self):
+        print("volando")
+        
+class Nadador:
+    def nadar(self):
+        print("nadando")
+        
+
+class Pato(Volador, Nadador, Caminador):     # Herencia multiple
+   def programar(self):
+        print("programando")
+        
+
